795aa00b978dd337eabbf6dfb94213789f619746
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-07-11 19:39:38 -0500

    Se suben cambios de nueva funcion con return de numero random hasta el numero que se quiere

--- src/hello-world/hello-world.module.ts
+++ src/hello-world/hello-world.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { HelloWorldResolver } from './hello-world.resolver';
-import { Query } from '@nestjs/graphql';
+import { Float, Int, Query } from '@nestjs/graphql';
 
 @Module({
   providers: [HelloWorldResolver]
 })
 export class HelloWorldModule {
 
-  @Query(() => String)
+  @Query(() => String, { name:'hello', description:'Function of Hello World' })
   helloWorld(): string {
     return 'Hola mundo';
   }
+
+  @Query(() => Float, { name:'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, { name:'randomNumberTo', description:'Function number random to number of function' })
+  getRandomFromZeroTo(): number {
+    return Math.floor(Math.random() * 10);
+  }
+
 
 }
